a743cb57ea553fc8c8259b5769f18c689e5c2918
sergey khvalko <falko.serega2016@yandex.ru>
2020-06-19 21:25:30 +0300

    initial commit

---
+++ src/js/views/currency.js
@@ -0,0 +1,23 @@
+class CurrencyUI {
+    constructor() {
+        this.currency = document.getElementById('currency');
+        this.dictionary = {
+        USD: '$',
+        EUR: '€',
+        };
+    }
+  
+    get currecyValue() {
+        return this.currency.value;
+    }
+  
+    getCurrencySymbol() {
+        console.log(this);
+        return this.dictionary[this.currecyValue];
+    }
+}
+  
+const currencyUI = new CurrencyUI();
+  
+export default currencyUI;
+  
